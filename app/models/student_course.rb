d5511eb309a47d9667075e62c62a3462733d1e86
mashood007 <masoodpookkadan@gmail.com>
2021-10-16 18:35:03 +0530

    default scope addedd as created_at

--- app/models/student_course.rb
+++ app/models/student_course.rb
@@ -3,6 +3,5 @@ class StudentCourse < ApplicationRecord
   belongs_to :course
   enum status: %I[not_attented passed failed]
   has_many :answer_sheets
-  # scope :passed, -> { where(status: :passed) }
   scope :appeared, -> { where.not(status: :not_attented) }
 end
